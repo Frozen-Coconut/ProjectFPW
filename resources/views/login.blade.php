b6a6037e0eb98ae2eb89b831db6645e0effa8af3
leonard3368 <51233664+leonard3368@users.noreply.github.com>
2022-11-23 19:54:49 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,21 +2,45 @@
 
 @section('body')
 @include('util.message')
-<h1 class="text-xl">Login (MASIH JELEK, JANGAN LUPA DIPERBAIKI)</h1>
-<a href="https://v1.tailwindcss.com/components/forms">https://v1.tailwindcss.com/components/forms</a>
-<form action="{{route('doLogin')}}" method="POST">
-    @csrf
-    <label for="email">Email</label><br>
-    <input type="text" name="email" id="email" value="{{old('email')}}"><br>
-    @error('email')
-        <p class="text-red-500">{{$message}}</p>
-    @enderror
-    <label for="password">Password</label><br>
-    <input type="password" name="password" id="password"><br>
-    @error('password')
-        <p class="text-red-500">{{$message}}</p>
-    @enderror
-    <button type="submit" name="login">Login</button>
-</form>
-<a href="{{route('register')}}">Menuju Register</a>
+
+<div class="w-full place-content-center flex drop-shadow-sm">
+    <div class="max-w-xs flex-col mt-20">
+        <form action="{{route('doLogin')}}" method="POST" class="border-solid border-gray-300 border rounded-lg p-10">
+            <h1 class="text-xl pb-5">Login</h1>
+            @csrf
+              <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                  Email
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="email" value="{{old('email')}}" type="text" placeholder="Email">
+                @error('email')
+                <p class="text-red-500">{{$message}}</p>
+                @enderror
+              </div>
+              <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                  Password
+                </label>
+                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="password" type="password" placeholder="Password">
+                @error('password')
+                <p class="text-red-500">{{$message}}</p>
+                @enderror
+              </div>
+              <div class="flex items-center justify-between">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" name="login">
+                  Sign In
+                  <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                </button>
+                  Forgot Password?
+                </a>
+              </div>
+              <div>
+                <a class="inline-block align-baseline text-sm text-blue-500 hover:text-blue-800 mt-7" href="{{route('register')}}">
+                    Don't have an account? Register now!
+                  </a>
+              </div>
+            </form>
+        </div>
+</div>
+
 @endsection
