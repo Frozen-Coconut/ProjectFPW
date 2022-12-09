fe3dd965dbee783619822ef57d1c3c19b57e7508
weifuk <weifuk678@gmail.com>
2022-12-09 21:37:58 +0700

    Update layout-daftar-tugas.blade.php

--- resources/views/project/ajax-layout/layout-daftar-tugas.blade.php
+++ resources/views/project/ajax-layout/layout-daftar-tugas.blade.php
@@ -29,7 +29,7 @@
         </div>
         <div class="flex justify-start items-center">
             @if ($value->deadline < $tgl_sekarang)
-            <button onclick="notify({{$value->id}})" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Notify</button>
+            <button onclick="notify({{$value->id}})" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Peringatkan</button>
             @endif
         </div>
     </div>
