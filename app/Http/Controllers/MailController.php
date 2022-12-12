12592fea97de70559c314538edc6c330313000d5
leonard3368 <51233664+leonard3368@users.noreply.github.com>
2022-12-12 12:47:10 +0700

    added mail

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\VerificationMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class MailController extends Controller
+{
+    function preview(Request $request){
+        return new VerificationMail();
+    }
+
+    function kirim(){
+        Mail::to('liantoleonard9@gmail.com')->send(new VerificationMail);
+    }
+}
